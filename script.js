da7a2809cb49b88059d9e17b7ea14e6b010ad402
Crows_Veldt <Snorri.Jonas@gmail.com>
2016-11-19 14:17:46 +0200

    Added Logo images to about page
    also removed unneeded comments from the Javascript file

---
+++ script.js
@@ -0,0 +1,13 @@
+//TODO: Animate the portfolio section, so when the user clicks on an image the space between the image-rows opens up and shows details about the projects. Also make the other project images more opaque.
+
+
+// function project (){
+//   document.getElementsByClassName('project').addEventListener('click', function(e) {
+//    if(document.getElementById('project1').className === 'projectdetailshidden'){
+//      document.getElementById('project1').className = 'projectdetailsshown';
+//     } else {
+//      document.getElementById('project1').className = 'projectdetailshidden';
+//     }
+//    });
+// }
+//
