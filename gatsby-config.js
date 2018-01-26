a592bd0179e854e8b7c8c05d8b4c668eae9885e1
CrowsVeldt <zechy.mahler@gmail.com>
2018-01-26 17:22:34 +0200

    Add Gatsby plugins
    and update .gitignore to ignore vim's .swp files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: 'My Page'
+    title: 'My Page',
+    author: 'Zechariah Mahler'
   },
-  plugins: ['gatsby-plugin-react-helmet']
+  plugins: [
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    'gatsby-transformer-remark'
+  ]
 }
