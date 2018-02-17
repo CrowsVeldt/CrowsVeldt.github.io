b5543153e57159bd330c05a22f23993dcfc46d0e
CrowsVeldt <zechy.mahler@gmail.com>
2018-02-17 11:19:14 +0200

    Make Tags page fit site style

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -2,12 +2,35 @@ import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import React from 'react'
 
-const TagsPage = ({ data: { allMarkdownRemark: { group }, site: { siteMetadata: { title } } } }) => (
-  <div>
-    <Helmet title={title} />
-    <div>
-      <h1>Tags</h1>
-      <ul>
+const TagsPage = ({ data: { allMarkdownRemark: { group } } }) => (
+  <div
+    className='tags-container'
+    css={{
+      padding: '3rem 1rem 3rem 1rem',
+      width: '100%',
+      maxWidth: '800px'
+    }}
+  >
+    <Helmet title={`TTID - Tags`} />
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      <h1
+        css={{
+          textAlign: 'center',
+          borderBottom: '2px groove black'
+        }}
+      >
+        Tags
+      </h1>
+      <ul
+        css={{
+          alignSelf: 'center'
+        }}
+      >
         {group.map(tag => (
           <li key={tag.fieldValue}>
             <Link to={`/tags/${tag.fieldValue}/`}>
@@ -24,11 +47,6 @@ export default TagsPage
 
 export const query = graphql`
  query TagsQuery {
-   site {
-     siteMetadata {
-       title
-     }
-   }
    allMarkdownRemark(
      limit: 2000
    ) {
