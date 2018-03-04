7c3f047e81b746e21386fee1d669086a86cc33b0
CrowsVeldt <zechy.mahler@gmail.com>
2018-03-04 15:31:46 +0200

    Combine postTemplate and blogPageTemplate
    re: Issue #51

--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -1,9 +1,81 @@
-import BlogPostTemplate from './blogPostTemplate'
+import ContentContainer from '../components/Page/ContentContainer'
+import PageNavLinks from '../components/Page/PageNavLinks'
+import Link from 'gatsby-link'
 import React from 'react'
+import Helmet from 'react-helmet'
 
 export default function Template ({ data, pathContext }) {
   const { markdownRemark: post } = data
-  return <BlogPostTemplate data={post} context={pathContext} />
+  const nextPage = pathContext.prev.frontmatter
+  const previousPage = pathContext.next.frontmatter
+
+  return (
+    <ContentContainer className='blog-post'>
+      <Helmet title={`TTID - ${post.frontmatter.title}`} />
+      <h1
+        css={{
+          fontSize: '2rem',
+          borderBottom: '2px groove black'
+        }}
+      >
+        {post.frontmatter.title}
+      </h1>
+      <p>
+        Published: {post.frontmatter.date}
+      </p>
+      <div
+        className='tag-container'
+        css={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignContent: 'center',
+          marginBottom: '1rem'
+        }}
+      >
+        <p
+          css={{
+            marginTop: 'auto',
+            marginBottom: 'auto'
+          }}
+        >
+          Tags:
+        </p>
+        <span
+          css={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            flexWrap: 'wrap'
+          }}
+        >
+          {post.frontmatter.tags.map((tag, index) => {
+            return (
+              <Link
+                to={`/tags/${tag}/`}
+                key={index}
+                css={{
+                  marginRight: '0.2rem',
+                  marginLeft: '0.2rem'
+                }}
+              >
+                {tag}
+              </Link>
+            )
+          })}
+        </span>
+      </div>
+      <div
+        className='blog-post-content'
+        css={{
+          width: '100%',
+          textAlign: 'start'
+        }}
+        dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      <PageNavLinks previous={previousPage} next={nextPage} />
+    </ContentContainer>
+  )
 }
 
 export const query = graphql`
